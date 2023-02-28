ec67d80968e8ff0ce3490be8497fc751fb164aa4
Tomasz Suski <tomasz.suski@orba.co>
2023-02-28 11:57:52 +0100

    added users list display

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <div>
       <AddUser addUsers={setUsers} users={users} />
-      <UsersList />
+      <UsersList users={users} setUsers={setUsers}/>
     </div>
   );
 }
--- src/Components/UsersList.js
+++ src/Components/UsersList.js
@@ -1,7 +1,23 @@
 import React from "react";
-import PropTypes from 'prop-types';
-import styles from './UsersList.module.css';
+import PropTypes from "prop-types";
+import styles from "./UsersList.module.css";
+import Card from "./UI/Card";
 
-export default function UsersList() {
-    return null;
+export default function UsersList({ users, setUsers }) {
+  return (
+    <Card className={styles.users}>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            {user.name} ({user.age} years old)  
+          </li>
+        ))}
+      </ul>
+    </Card>
+  );
 }
+
+UsersList.propTypes = {
+  users: PropTypes.array.isRequired,
+  setUsers: PropTypes.func.isRequired,
+};
