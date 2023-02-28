532c2224bbd63f721f02c75d8d3b853daaa289ea
Tomasz Suski <tomasz.suski@orba.co>
2023-02-28 10:09:09 +0100

    added input listening and clearing after submit

--- src/Components/Users/AddUser.js
+++ src/Components/Users/AddUser.js
@@ -1,27 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import styles from "./AddUser.module.css";
 import Card from "../UI/Card";
 import Button from "../UI/Button";
 
 export default function AddUser({ addUsers, users }) {
+  const [userName, setUserName] = useState("");
+  const [userAge, setUserAge] = useState();
+  const userNameChangeHandler = (event) => {
+    setUserName(event.target.value);
+  };
+  const userAgeChangeHandler = (event) => {
+    setUserAge(event.target.value);
+  };
   const addUserHandler = (event) => {
     event.preventDefault();
     let newUser = {
-      name: event.target.username.value,
-      age: event.target.age.value,
-      id: Math.random() + event.target.username.value + event.target.age.value,
+      name: userName,
+      age: userAge,
+      id: Math.random() + userName + userAge,
     };
     let newUsers = [newUser, ...users];
     addUsers(newUsers);
+    setUserName("");
+    setUserAge("");
   };
   return (
     <Card className={styles.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input id="username" type="text" />
+        <input
+          id="username"
+          type="text"
+          value={userName}
+          onChange={userNameChangeHandler}
+        />
         <label htmlFor="age">Age (Years)</label>
-        <input id="age" type="number" />
+        <input
+          id="age"
+          type="number"
+          value={userAge}
+          onChange={userAgeChangeHandler}
+        />
         <Button type="submit">Add User</Button>
       </form>
     </Card>
